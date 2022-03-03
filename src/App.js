3d48e6f55d6d34fa54098c8a200ec609f2d6e93b
Alfio Federico Di Pinto <fefferico81@gmail.com>
2022-03-03 14:40:38 +0900

    Main page completed with API

--- src/App.js
+++ src/App.js
@@ -4,16 +4,98 @@ import SearchTwoToneIcon from "@mui/icons-material/SearchTwoTone";
 import Country from "./components/Country";
 import { Routes, Route } from "react-router-dom";
 import CountryDetails from "./components/CountryDetails";
-import {useState} from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
+ const [darkMode, setDarkMode] = useState(false);
+
+ const [countries, setCountries] = useState([]);
+
+ const countriesInputRef =useRef()
+ const regionRef = useRef()
+
+ const noResults = countries.status || countries.message
 
   const switchMode = () => {
     setDarkMode(prevState => !prevState)
   }
 
-  const [darkMode, setDarkMode] = useState(false)
+useEffect(() =>{
+  try {
+    fetchData()
+  } catch (error) {
+    console.log(error)
+  }
+}, [])
 
+ const fetchData = async () =>{
+   const res = await fetch("https://restcountries.com/v2/all");
+   const data = await res.json()
+
+  if(data.status ===400){
+    setCountries([])
+    return
+  }
+   setCountries(data)
+ }
+
+const searchCountries = () =>{
+  const serachValue = countriesInputRef.current.value
+
+  if (serachValue.trim()) {
+    const fetchSearch = async () => {
+      const response = await fetch(
+        `https://restcountries.com/v2/name/${serachValue}`
+      );
+      const data = await response.json();
+
+      setCountries(data);
+    };
+
+    try {
+      fetchSearch();
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    fetchData();
+  }
+}
+
+
+const selectRegion = () => {
+  const selectValue = regionRef.current.value
+
+  if(selectValue.trim()){
+    const fetchSelect = async()=>{
+      const response = await fetch(
+        `https://restcountries.com/v2/region/${selectValue}`
+      );
+      const data = await response.json()
+
+
+      if(selectValue ==='All') {
+        try {
+          fetchData()
+        } catch (error) {
+          console.log(error)
+        }
+        return
+      }
+      setCountries(data)
+    }
+
+    try {
+      fetchSelect()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+const showDetails = (code) => {
+  
+}
   return (
     <div className={`app ${darkMode ? "darkMode" : ""}`}>
       <Header onClick={switchMode} darkMode={darkMode} />
@@ -26,13 +108,13 @@ function App() {
               <div className="inputs">
                 <div className={`search-input ${darkMode ? "darkMode" : ""}`}>
                   <SearchTwoToneIcon />
-                  <input type="text" placeholder="Search for a country..." />
+                  <input type="text" placeholder="Search for a country..." ref={countriesInputRef} onChange={searchCountries}/>
                 </div>
                 <div className={`filter-region ${darkMode ? "darkMode" : ""}`}>
-                  <select>
+                  <select ref={regionRef} onChange={selectRegion}>
                     <option>All</option>
-                    <option>America</option>
-                    <option>Europa</option>
+                    <option>Americas</option>
+                    <option>Europe</option>
                     <option>Asia</option>
                     <option>Africa</option>
                     <option>Oceania</option>
@@ -41,7 +123,21 @@ function App() {
               </div>
 
               <div className="countries">
-                <Country darkMode={darkMode} />
+                {!noResults ? (countries.map((country) => (
+                  <Country
+                    darkMode={darkMode}
+                    key={country.cca3}
+                    code={country.cca3}
+                    name= {country.name}
+                    capital = {country.capital}
+                    population = {country.population}
+                    region = {country.region}
+                    flag= {country.flag}
+                    showDetails = { showDetails }
+                  />
+                ))) : (
+                  <p>No countries found</p>
+                )}
               </div>
             </div>
           }
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,24 +1,30 @@
 import React from 'react'
 
-const Country = ({darkMode}) => {
+const Country = ({darkMode, population, flag, capital, name, region}) => {
   return (
     <div className={`country ${darkMode ? "darkMode" : ""}`}>
-      <div className="flag-conteiner">
-        <img src="" alt="Flags" />
+      <div className="flag-container">
+        <img src={flag} alt="Flags" />
       </div>
       <div className="details">
-        <h3 className={`name ${darkMode ? "darkMode" : ""}`}>Name</h3>
+        <h3 className={`name ${darkMode ? "darkMode" : ""}`}>{name}</h3>
         <p>
           Population: {""}
-          <span className={`values ${darkMode ? "darkMode" : ""}`}>Test</span>
+          <span className={`values ${darkMode ? "darkMode" : ""}`}>
+            {population.toLocaleString()}{" "}
+          </span>
         </p>
         <p>
           Region: {""}
-          <span className={`values ${darkMode ? "darkMode" : ""}`}>Test</span>
+          <span className={`values ${darkMode ? "darkMode" : ""}`}>
+            {region}
+          </span>
         </p>
         <p>
           Capital: {""}
-          <span className={`values ${darkMode ? "darkMode" : ""}`}>Test</span>
+          <span className={`values ${darkMode ? "darkMode" : ""}`}>
+            {capital}
+          </span>
         </p>
       </div>
     </div>
